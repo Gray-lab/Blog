cc1b12aa423593c84e887376d8c136017a8dfca9
Gray-lab <martin.grym@gmail.com>
2022-09-28 14:40:22 -0500

    Added Post model

--- graylab/blog/models.py
+++ graylab/blog/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=64, unique=True)
+  content = models.TextField()
+  date_created = models.DateTimeField(auto_now_add = True)
+  date_modified = models.DateTimeField(auto_now = True)
--- graylab/blog/views.py
+++ graylab/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, world! You are at the gray-lab blog index.")
+  return render(request, "blog/index.html")
+
+
