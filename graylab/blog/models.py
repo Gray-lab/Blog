223399aee0f8b5a4d73732d04f405d233332afe1
Gray-lab <martin.grym@gmail.com>
2022-09-28 23:54:17 -0500

    added model and some test entries

--- graylab/blog/models.py
+++ graylab/blog/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
   content = models.TextField()
   date_created = models.DateTimeField(auto_now_add = True)
   date_modified = models.DateTimeField(auto_now = True)
+
+  def __str__(self):
+    return f'{self.title} written on {self.date_created}'
