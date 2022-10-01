8faa6f5f1cdffb08f3c446d08b3f69ee579a234a
Gray-lab <martin.grym@gmail.com>
2022-10-01 13:44:10 -0500

    added project and home apps

--- graylab/blog/models.py
+++ graylab/blog/models.py
@@ -10,3 +10,4 @@ class Post(models.Model):
 
   def __str__(self):
     return f'{self.title} written on {self.date_created}'
+
--- graylab/blog/views.py
+++ graylab/blog/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Post
+from markdown2 import Markdown
 
 # Create your views here.
 def index(request):
-  return render(request, "blog/index.html")
+  
+  return render(request, "blog/index.html", {
+    "posts": Post.objects.all()
+  })
 
 
+def entry(request, id):
+    # check if page exists, and if not, render an error page
+    post = Post.objects.filter(id = id).first()
+    markdown_content = post.content
+    title = post.title
+    # parse markdown to HTML before passing it as a variable to the renderer
+    markdowner = Markdown()
+    content = markdowner.convert(markdown_content)
+    return render(request, "blog/post.html", {
+        "title": title, # Passes in title to html renderer, to be used in the template
+        "content": content
+    })
